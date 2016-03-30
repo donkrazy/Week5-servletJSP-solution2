6ea97b53fd4c95ce5e276c367a0f43ff97fb73fd
kickscar <kickscar@gmail.com>
2016-03-30 14:37:19 +0900

    loginform joinsuccess 액션 처리

--- src/com/estsoft/mysite/web/action/user/LoginFormAction.java
+++ src/com/estsoft/mysite/web/action/user/LoginFormAction.java
@@ -9,12 +9,11 @@ import javax.servlet.http.HttpServletResponse;
 import com.estsoft.web.WebUtil;
 import com.estsoft.web.action.Action;
 
-public class JoinAction implements Action {
+public class LoginFormAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		/* Dao 처리 */
-		WebUtil.redirect(request, response, "/mysite/user?a=joinsuccess");
+		WebUtil.forward(request, response, "/WEB-INF/views/user/loginform.jsp");
 	}
 
 }
